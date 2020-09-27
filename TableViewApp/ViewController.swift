be0601fb3adfc5afa5454f59606afc13cce804a6
Ali Ertekin Elkoca <30657493+ertekinelkoca@users.noreply.github.com>
2020-09-28 01:06:40 +0300

    second commit / tableview design

--- TableViewApp/ViewController.swift
+++ TableViewApp/ViewController.swift
@@ -7,11 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var racingNames = [String]()
+    var racingImages = [UIImage]()
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        //DATAS
+        racingNames.append("cbr1000rrsp")
+        racingNames.append("gsxr1000")
+        racingNames.append("h2")
+        racingNames.append("HP4")
+        racingNames.append("yzfr1")
+        racingNames.append("zx10r")
+        
+        racingImages.append(UIImage(named: "cbr1000rrsp")!)
+        racingImages.append(UIImage(named: "gsxr1000")!)
+        racingImages.append(UIImage(named: "h2")!)
+        racingImages.append(UIImage(named: "HP4")!)
+        racingImages.append(UIImage(named: "yzfr1")!)
+        racingImages.append(UIImage(named: "zx10r")!)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return racingImages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = racingNames[indexPath.row]
+        return cell
     }
 
 
